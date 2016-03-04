3bd1689d180a867e099fcc7927aff7ca6ff759da
Jeremy Smith <smithjeremym@gmail.com>
2016-03-04 15:31:57 -0500

    get chessboard layout built out

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+// $(document).ready( function () {
+//   "use strict";
+// not sure if these two jquery initiators are interchangeable. Probably not, but they prbably both work for this project
+
+$(function () {
+  "use strict";
+
+
+
+
+
+});
