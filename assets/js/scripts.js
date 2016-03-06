dbf66100f076e7b0cf6940a338d96a1a42f99df3
Jeremy Smith <smithjeremym@gmail.com>
2016-03-06 16:43:54 -0500

    build out move functionality and correct tile coloring

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,74 @@
-// $(document).ready( function () {
-//   "use strict";
-// not sure if these two jquery initiators are interchangeable. Probably not, but they prbably both work for this project
-
-$(function () {
+$(document).ready( function () {
+// $( function(){ this could work as well!
   "use strict";
 
-  //I should probably declare variables for the chess pieces according to their unicode counteparts
-  // name          symbol unicode html
-  // white chess king	♔	U+2654	&#9812;
-  // white chess queen	♕	U+2655	&#9813;
-  // white chess rook	♖	U+2656	&#9814;
-  // white chess bishop	♗	U+2657	&#9815;
-  // white chess knight	♘	U+2658	&#9816;
-  // white chess pawn	♙	U+2659	&#9817;
-  // black chess king	♚	U+265A	&#9818;
-  // black chess queen	♛	U+265B	&#9819;
-  // black chess rook	♜	U+265C	&#9820;
-  // black chess bishop	♝	U+265D	&#9821;
-  // black chess knight	♞	U+265E	&#9822;
-  // black chess pawn	♟	U+265F	&#9823;
+  // declare variables for each unicode character key
 
-  var whitePawn = "&#9817";
-
-  //white pieces have capital letters; black pieces lower case
+  var wKing = "&#9812";
+  var wQueen = "&#9813";
+  var wRook = "&#9814";
+  var wBishop = "&#9815";
+  var wKnight = "&#9816";
+  var wPawn = "&#9817";
+  var bKing = "&#9818";
+  var bQueen = "&#9819";
+  var bRook = "&#9820";
+  var bBishop = "&#9821";
+  var bKnight = "&#9822";
+  var bPawn = "&#9823";
 
   //should I put all of the moves into an array? That way, I can just call moves[1] when the first move happens, moves[2] with the second, moves[0] for the beginning, etc...
   var newGame = [
-    ["R","N","B","Q","K","B","N","R"],
-    ["P","P","P","P","P","P","P","P"],
+    [wRook,wKnight,wBishop,wQueen,wKing,wBishop,wKnight,wRook],
+    [wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn,wPawn],
     [" "," "," "," "," "," "," "," "],
     [" "," "," "," "," "," "," "," "],
     [" "," "," "," "," "," "," "," "],
     [" "," "," "," "," "," "," "," "],
-    ["p","p","p","p","p","p","p","p"],
-    ["r","n","b","q","k","b","n","r"]
+    [bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn],
+    [bRook,bKnight,bBishop,bQueen,bKing,bBishop,bKnight,bRook]
   ];
 
-  //[1][5] = " "
-  //[3][5] = "P"
   var firstMove = [
-    ["R","N","B","Q","K","B","N","R"],
-    ["P","P","P","P"," ","P","P","P"],
+    [wRook,wKnight,wBishop,wQueen,wKing,wBishop,wKnight,wRook],
+    [wPawn,wPawn,wPawn,wPawn," ",wPawn,wPawn,wPawn],
     [" "," "," "," "," "," "," "," "],
-    [" "," "," "," ",whitePawn," "," "," "],
+    [" "," "," "," ",wPawn," "," "," "],
     [" "," "," "," "," "," "," "," "],
     [" "," "," "," "," "," "," "," "],
-    ["p","p","p","p","p","p","p","p"],
-    ["r","n","b","q","k","b","n","r"]
+    [bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn,bPawn],
+    [bRook,bKnight,bBishop,bQueen,bKing,bBishop,bKnight,bRook]
   ];
+
+  //this runs upon page load and populates the board with the initial positions of the pieces
+  for (var i=0; i < firstMove.length; i++){
+    // console.log("i is " + i);
+    for (var I=0; I < newGame.length; I++){
+      console.log("i is " + i);
+      console.log("I is " + I);
+      // console.log("firstMove[i][I] is " + firstMove[i][I]);
+      // $(".square").html(firstMove[i][I]);//this did not work :) it put r in every square
+      // console.log("firstMove[i][I+1] is " + firstMove[i][I]);
+      $(".chessBoard").append('<div class="square">'+newGame[i][I]+'</div>');
+
+      // if i is even AND I is odd, it's a dark square OR if i is odd AND I is even, it's a dark square
+      // else, it's a light square
+      console.log(i*8 + I);//this gives a unique identifier to each square, starting at 0
+      // this function provides the correct colors to the board squares
+      if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){
+        $(".square").eq(i*8 + I).addClass("dkSquare");
+      } else {
+        //do nothing
+      }
+        //  || i % 2 !== 0 && I % 2 === 0
+      // } else {
+        //do nothing
+      // }
+
+
+    }
+
+  }
 
   $("#rewind").on("click", function () {
     console.log("rw");
@@ -71,12 +92,17 @@ $(function () {
     // put the current Move
     // do the nested for loop thing? I kinda want to try it. It should work, but there's a better way to do it.
     for (var i=0; i < firstMove.length; i++){
-      // console.log("i is " + i);
-      for (var I=0; I < firstMove[i].length; I++){
-        // console.log("I is " + I);
-        console.log("firstMove[i][I] is " + firstMove[i][I]);
-        $(".square").html(firstMove[i][I]);//this did not work :) it put r in every square
-        //$(".square").nthSibling(I).html(firstMove[i][I]);??????
+      console.log("i is " + i);
+      for (var I=0; I < firstMove[i].length; I+=2){
+        console.log("I is " + I);
+        // console.log("firstMove[i][I] is " + firstMove[i][I]);
+        // $(".square").html(firstMove[i][I]);//this did not work :) it put r in every square
+        // console.log("firstMove[i][I+1] is " + firstMove[i][I]);
+        $(".chessBoard").append('<div class="square">'+firstMove[i][I]+'</div><div class="square">'+firstMove[i][I+1]+'</div>');
+
+        // $(".square").
+
+        // <div class='square dkSquare'>"+firstMove[i][I+1]+"</div>
       }
 
     }
