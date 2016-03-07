6d4a4e6c12408e37920159da4f41e1e4758ac2d9
Jeremy Smith <smithjeremym@gmail.com>
2016-03-06 19:58:33 -0500

    get play button to play sequence of moves

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready( function () {
-// $( function(){ this could work as well!
+// $( function(){ could work as well!
   "use strict";
 
   // declare variables for each unicode character key
-
+  // https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
   var wKing = "&#9812";
   var wQueen = "&#9813";
   var wRook = "&#9814";
@@ -121,7 +121,7 @@ $(document).ready( function () {
     ],
   ];
 
-  var move = 0;//this variable is going to keep track of what move of the game we're on through the various steps forward/back/etc...
+  var move = 0;//this global variable is going to keep track of what move of the game we're on through the various steps forward/back/etc...
 
   //this runs upon page load and populates the board with the initial positions of the pieces
   for (var i=0; i < 8; i++){ // iterates through the rows
@@ -130,7 +130,7 @@ $(document).ready( function () {
       $(".chessBoard").append('<div class="square">'+gamePlay[move][i][I]+'</div>');//populate the board with squares and chess pieces
       // this populates the correct colors to the board squares
       if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){// if i is even AND I is odd, it's a dark square OR if i is odd AND I is even, it's a dark square
-        $(".square").eq(i*8 + I).addClass("dkSquare");//(i*8 + I) gives a unique identifier (from 0 to 63) to each square
+        $(".square").eq(i*8 + I).addClass("dkSquare");//(i*8 + I) gives a unique identifier (from 0 to 63) to each square and eq targets the nth element in the category where n is .eq(n)
       } else {
         //do nothing; it's a light square
       }
@@ -146,10 +146,6 @@ $(document).ready( function () {
     for (var i=0; i < 8; i++){
       for (var I=0; I < 8; I++){
         $(".square").eq(i*8 + I).html(gamePlay[move][i][I]);//populate the tiles with the correct pieces
-        if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){
-          $(".square").eq(i*8 + I).addClass("dkSquare");
-        } else {
-        }
       }
     }
 
@@ -169,21 +165,35 @@ $(document).ready( function () {
     for (var i=0; i < 8; i++){
       for (var I=0; I < 8; I++){
         $(".square").eq(i*8 + I).html(gamePlay[move][i][I]);//populate the tiles with the correct pieces
-        if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){
-          $(".square").eq(i*8 + I).addClass("dkSquare");
-        } else {
-        }
       }
     }
   });
 
-  $("#play").on("click", function () {
-    console.log("play");
-    //play each move, with a ~1 second delay between the moves
+  $("#play").on("click", function (play) {
+    // play through the remaining sequence, with a delay between each move
+
+    setInterval(function() { //do what's inside this thing
+      if (move < gamePlay.length - 1){ // only play next moves when you can
+        move += 1; // add 1 to the move counter (global variable)
+      } else {
+        // there are no moves to make.
+        play.stopPropagation(); // this doesnt work
+      }
+
+      for (var i=0; i < 8; i++){
+        for (var I=0; I < 8; I++){
+          $(".square").eq(i*8 + I).html(gamePlay[move][i][I]);//populate the tiles with the correct pieces
+        }
+      }
+    }, 750); // every 3/4ths of a second
+
+  // this didn't work ... ASK SOMEBODY HOW TO DO IT!
+
+    // play.stopPropagation(); // Pause the sequence
+
   });
 
   $("#stepForward").on("click", function () {
-
     if (move < gamePlay.length - 1){ // only move forward when you can
       move += 1; // add 1 to the move counter (global variable)
     } else {
@@ -196,10 +206,6 @@ $(document).ready( function () {
     for (var i=0; i < 8; i++){
       for (var I=0; I < 8; I++){
         $(".square").eq(i*8 + I).html(gamePlay[move][i][I]);//populate the tiles with the correct pieces
-        if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){
-          $(".square").eq(i*8 + I).addClass("dkSquare");
-        } else {
-        }
       }
     }
   });
@@ -213,17 +219,9 @@ $(document).ready( function () {
     for (var i=0; i < 8; i++){
       for (var I=0; I < 8; I++){
         $(".square").eq(i*8 + I).html(gamePlay[move][i][I]);//populate the tiles with the correct pieces
-        if (i % 2 === 0 && I % 2 !== 0 || i % 2 !== 0 && I % 2 === 0){
-          $(".square").eq(i*8 + I).addClass("dkSquare");
-        } else {
-        }
       }
     }
 
   });
-
-
-
-
 
 });
